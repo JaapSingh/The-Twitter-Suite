d035f1ff85130b2b682d65a9a9a9cf5e41de7773
JaapSingh <harjaapsinghgill@gmail.com>
2016-01-19 01:23:08 -0500

    done

--- First Bot/helloworld.py
+++ First Bot/helloworld.py
@@ -6,10 +6,10 @@ import tweepy, time, sys
 argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = '9gqIhQvRcKaCnszpPIJQRomNF'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'Zgf9RMpDDVeDMQaRhA5OebdaQl8TqRiQ6qhLfZ5dSFfYmryaxS'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '3659277502-I1Wx27SQln9Hek71160ziRmMcsHwjcxos9JfWrH'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'skxxEnSjuVom4SNzRGhS7eX60cE4uqulUQtpslmSsC6LB'#keep the quotes, replace this with your access token secret
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
